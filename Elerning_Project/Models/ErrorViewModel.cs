bd7235391ed91c4266060e6080c1c51c4be38c60
DoDinhLoc <dinhloc0710@gmail.com>
2022-06-02 22:55:46 +0700

    Add project files.

---
+++ Elerning_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Elerning_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
